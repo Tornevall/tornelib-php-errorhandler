508a28a1eff34ad0ee83418d0fdc13f0ad049289
Tomas Tornevall <thorne@tornevall.net>
2020-02-04 14:59:26 +0100

    PreErrorHandler

---
+++ src/Exception/Constants.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace TorneLIB\Exception;
+
+abstract class Constants
+{
+    /** @var int No error constant. For testing purposes. */
+    const LIB_NO_ERROR = 0;
+
+    const LIB_UNHANDLED = 65535;
+}
---
+++ src/Exception/ExceptionHandler.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace TorneLIB\Exception;
+
+class ExceptionHandler extends \Exception {
+
+    public function __construct(
+        $message = 'Unknown exception',
+        $code = 0,
+        \Exception $previous = null,
+        $stringifiedCode = null,
+        $fromFunction = ''
+    ) {
+        if (!$code) {
+            if (!defined('LIB_ERROR_HTTP')) {
+                // Use internal error.
+                $code = Constants::LIB_UNHANDLED;
+            } else {
+                // Use bad request when unknown and LIB_ERROR_HTTP is enabled.
+                $code = 400;
+            }
+        }
+        parent::__construct($message, $code, $previous);
+        $this->traceFunction = $fromFunction;
+        $this->stringifiedCode = $stringifiedCode;
+        $this->setStringifiedCode();
+    }
+
+    /**
+     *
+     */
+    private function setStringifiedCode()
+    {
+        if (empty($this->code) && !empty($this->stringifiedCode)) {
+            try {
+                $constant = constant('CONSTANTS::' . $this->stringifiedCode);
+            } catch (\Exception $regularConstantException) {
+                // Ignore this.
+            }
+            if (!empty($constant)) {
+                $this->code = constant('CONSTANTS::' . $this->stringifiedCode);
+            } else {
+                $this->code = $this->stringifiedCode;
+            }
+        }
+    }
+
+    public function __toString()
+    {
+        if (empty($this->traceFunction)) {
+            return "Exception: [{$this->code}]: {$this->message}";
+        } else {
+            return "{$this->traceFunction}Exception {$this->code}: {$this->message}";
+        }
+    }
+
+    /**
+     * @return |null
+     */
+    public function getStringifiedCode()
+    {
+        return $this->stringifiedCode;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTraceFunction()
+    {
+        return $this->traceFunction;
+    }
+}
