54c33d6ca8ad53d5752e60d916fa6315ab2853ba
Tomas Tornevall <thorne@tornevall.net>
2020-03-17 22:12:26 +0100

    Errors for invalid urls.

--- src/Exception/Constants.php
+++ src/Exception/Constants.php
@@ -9,4 +9,10 @@ abstract class Constants
 
     /** @var int All unhandled error codes unless we choose to transform into HTTP return codes. */
     const LIB_UNHANDLED = 65535;
+
+    /** @var int Invalid URL. */
+    const LIB_INVALID_URL = 1002;
+
+    /** @var int Usually thrown when there's no URL present. */
+    const LIB_EMPTY_URL = 1002;
 }
